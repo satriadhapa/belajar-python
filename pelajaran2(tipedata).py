35c576bc63b749d7a3a7b52a9afcbfb132bc7fff
apollon <76041403+satriadhapa@users.noreply.github.com>
2022-03-01 14:08:09 +0700

    Add files via upload

---
+++ pelajaran2(tipedata).py
@@ -0,0 +1,35 @@
+# # cara mendapatkan type data dari sebuah isian variabel
+# a = 10
+# print(type(a))
+
+# macam macam tipe data
+a = "satria"
+b = 20
+c = 0.33
+d = 1j
+e = ["saria","dhapa","herculs"]
+f = ('satria', 3, "dhapa")
+g = range(10)
+h = {1 : "teknik informatika", 2: "teknik kimia"}
+i = {"satria", "dhapa", "hamdani"}
+j = frozenset({"apel", "pisang", "cawu"})
+k = True
+l = b"hello"
+m = bytearray(10)
+n = memoryview(bytes(4))
+
+print(type(a)) # <class 'str'>
+print(type(b)) # <class 'int'>
+print(type(c)) # <class 'float'>
+print(type(d))# <class 'complex'>
+print(type(e))# <class 'list'>
+print(type(f))# <class 'tuple'>
+print(type(g))# <class 'range'>
+print(type(h))# <class 'dict'>
+print(type(i))# <class 'set'>
+print(type(j))# <class 'frozenset'>
+print(type(k))# <class 'bool'>
+print(type(l))# <class 'bytes'>
+print(type(m))# <class 'bytearray'>
+print(type(n))# <class 'memoryview'>
+
