f120c9b1c7c5e9f6f634b412dfa3a80bb11d98f6
apollon <76041403+satriadhapa@users.noreply.github.com>
2022-03-02 11:39:58 +0700

    Add files via upload

---
+++ pelajaran6(operator).py
@@ -0,0 +1,68 @@
+# python operator 
+# terdapat macam macam operator dalam python
+
+# arithmetic operator
+# assignment operator
+# comparison operator
+# logical operator
+# identity operator
+# membership operator
+# bitwise operator
+
+# 1 arithmetic operator
+from tkinter import Y
+
+
+a1 = 1 + 2 # + addition
+a2 = 1 - 2 # - subsraction
+a3 = 3 * 3 # * / x multiplication
+a4 = 2 / 3 # / division
+a5 = 42 % 2 # % modulus
+a6 = 2 ** 2 # ** exponetitation
+a7 = 2 // 2 # floor division
+
+# 2 assignment operator
+b1 = 2 # = 
+b1 += 2 # +=
+b1 -= 2 # -=
+b1 *= 2 # *=
+b1 /= 2 # /=
+b1 %= 3 # %=
+b1 //= 2 # b1 //=
+b1 **= 23 # **=
+b1 &= 3 # &=
+b1 |= 2 # |=
+b1 ^= 2 # ^=
+b1 >>= 3 # >>=
+b1 <<= 3 # <<=
+
+# 3 comparison operator
+# == equal
+# != not equal
+# > lebih besar
+# < lebih kecil 
+# >= lebih besar sama dengan 
+# <= lebih kecil sama dengan
+
+# 4 logical comparison
+c1 = 2 and 2 # and mengembalikan nilai True jika kedua nilai nya sama
+c2 = 3 or 2 # or mengembaikan nilai True jika salah satu nilai terbukti benar
+x = 10
+c3 = not(x< 10 and x > 10) # not reverse the result , mengembalikan False Jika ada hasil yang True
+
+# 5 identity operator
+d1 = x is c1 # mengembalikan nilai True jika kedua variabel memiliki object yang sama
+d2 = x is not c2 # mengembalikan nilai True jika kedua variabel memiliki obj yang tidak sama
+
+# 6 membership operator
+e1 = a1 in b1 # in Returns True if a sequence with the specified value is present in the object
+e2 = a1 not in c2 # Returns True if a sequence with the specified value is not present in the object
+
+# 7 bitwisw operator
+# & AND
+# | OR
+# ^ XOR
+# ~ NOT
+# << zero fill left shift
+# >> signed right shift
+
